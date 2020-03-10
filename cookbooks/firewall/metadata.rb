379546f2e4c03da57235bbd2032688fd4a5bbe78
michael spreng <code@m.spreng.ch>
2020-03-10 08:48:44 +0100

    prepare firewall cookbook

---
+++ cookbooks/firewall/metadata.rb
@@ -0,0 +1,7 @@
+maintainer        "fossgis routing server"
+maintainer_email  "fossgis-routing-server@openstreetmap.de"
+license           "Apache 2.0"
+description       "Firewall configuration"
+long_description  IO.read(File.join(File.dirname(__FILE__), 'README.rdoc'))
+version           "1.0.0"
+name              "firewall"
